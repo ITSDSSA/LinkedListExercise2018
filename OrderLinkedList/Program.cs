14b9738bc71d8d75882b7b68f72b897d4f8feec0
ITSPaul <powell.paul@itsligo.ie>
2018-04-17 12:02:16 +0100

    Initial Commit

---
+++ OrderLinkedList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> l = new LinkedList<int>();
+
+            Insert(l, 7);
+            Insert(l, 2);
+            Insert(l, 3);
+            Insert(l, 8);
+
+            foreach (var item in l)
+            {
+                Console.WriteLine("{0}", item);
+            }
+            Console.ReadKey();
+        }
+
+        static void Insert(LinkedList<int> l, int item)
+        {
+            if (l.First == null)
+                l.AddFirst(item);
+            else if (item <= l.First.Value)
+                l.AddFirst(item);
+            else if (item >= l.Last.Value)
+                l.AddLast(item);
+            else
+            {
+                LinkedListNode<int> node = l.First;
+                while (item >= node.Value)
+                    node = node.Next;
+                l.AddBefore(node, item);
+
+            }
+        }
+    }
+}
